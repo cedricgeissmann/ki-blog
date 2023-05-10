7b5794619c880495b8d85b87a893a5259f715413
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-05-10 12:39:41 +0200

    Simple digits

--- digits.js
+++ digits.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('#canvas');
-canvas.width = 32
-canvas.height = 32
+canvas.width = 16
+canvas.height = 16
 const ctx = canvas.getContext('2d');
 
 let coords = {
@@ -16,7 +16,6 @@ document.addEventListener('mouseup', stop);
 function start(e) {
   reposition(e);
   document.addEventListener('mousemove', draw);
-  showCoords(coords.x, coords.y);
 }
 
 function stop() {
@@ -25,7 +24,6 @@ function stop() {
 
 
 function draw(e) {
-  showCoords(coords.x, coords.y);
   ctx.beginPath();
   ctx.lineWidth = 2
   ctx.strokeColor = 'black'
@@ -34,10 +32,6 @@ function draw(e) {
   ctx.lineTo(coords.x, coords.y);
   ctx.stroke();
   
-}
-
-function showCoords(x, y) {
-  document.querySelector('#coords').textContent = `x: ${x}, y: ${y}`;
 }
 
 function reposition(e) {
@@ -52,6 +46,93 @@ document.querySelector('#btn-clear').addEventListener('click', () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
-document.querySelector('#btn-guess').addEventListener('click', () => {
-  alert("The number is: " + Math.round(Math.random() * 10));
+document.querySelector('#btn-guess').addEventListener('click', async () => {
+      
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  const greyData = new Array(data.length / 4).fill(0);
+  for (let i = 0; i < data.length; i += 4) {
+    const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    greyData[i / 4] = avg;
+  }
+  console.log(greyData)
+  //alert("The number is: " + Math.round(Math.random() * 10));
+  const imgNew = new Image();
+  imgNew.src = canvas.toDataURL();
+  console.log(imgNew)
+
+  //  the actual classification using the trained classfier
+  const classification = await classifier.classify(imgNew);
+  console.log(classification);
 })
+
+document.querySelector('#btn-save').addEventListener('click', async (e) => {
+    let downloadLink = document.createElement('a');
+    downloadLink.setAttribute('download', 'CanvasAsImage.png');
+    canvas.toBlob(function(blob) {
+      let url = URL.createObjectURL(blob);
+      downloadLink.setAttribute('href', url);
+      downloadLink.click();
+    });
+})
+
+
+let featureExtractor;
+let classifier;
+
+const trainingImages = [];
+const NUM_TRAIN = 8;
+
+function preloadImages() {
+  console.log('Preloading images');
+  const trainingFolderUrl = 'images/training';
+  
+  const trainingFolders = new Array(NUM_TRAIN).fill(0).map((_, i) => i + 1);
+  const imagesNames = new Array(10).fill(0).map((_, i) => `${i}.png`);
+  
+  trainingFolders.forEach((folderName) => {
+    imagesNames.forEach((imageName, i) => {
+      const imageUrl = `${trainingFolderUrl}/${folderName}/${imageName}`;
+      trainingImages.push({ image: createImg(imageUrl), label: i });
+    })
+  })
+}
+
+function createImg(url) {
+  const img = new Image();
+  img.src = url;
+  return img;
+}
+
+function trainModel() {
+  const promises = [];
+  trainingImages.forEach((dp) => {
+    promises.push(classifier.addImage(dp.image, dp.label));  
+  });
+
+  return Promise.all(promises);
+}
+
+async function modelLoaded() {
+  classifier = featureExtractor.classification();
+
+  //  add new train image for every image/label pair stored in `trainingImages`
+  await trainModel();
+  
+  // Retrain the network 
+  console.log('training model')
+  await classifier.train(function(lossValue) {
+    console.log("Loss is", lossValue);
+  });
+  console.log("Training done")
+
+}
+
+function setup() {
+  console.log("setup");
+  preloadImages()
+  featureExtractor = ml5.featureExtractor("MobileNet", { numLabels: 10 }, modelLoaded);
+  console.log("setup done");
+}
+
+document.addEventListener('DOMContentLoaded', setup);
